8eba582a10b0231a7e7c96dbe4fdf61dd4bac459
mihal.d44 <mihal.d44@gmail.com>
2023-07-03 13:55:31 +0300

    comment.py revision

--- src/widgets/comment.py
+++ src/widgets/comment.py
@@ -19,9 +19,14 @@ class Comment(QWidget):
         self.ui = Ui_comment()
 
         self.ui.setupUi(self)
+        self.ui.edit.setIcon(icons.get_other_icon("toEdit"))
+        self.ui.remove.setIcon(icons.get_other_icon("cancel2"))
         self.ui.textEdit.hide()
 
-        self.height_ = MIN_HEIGHT
+        self.visible_height = MIN_HEIGHT
+        self.expanded_height = 0
+        # logger.info(f'{id=}, {self.visible_height=}')
+
         self.id = id
 
         self.ui.collapse.clicked.connect(self.collapse_item)
@@ -29,21 +34,24 @@ class Comment(QWidget):
         self.ui.remove.clicked.connect(self.remove_note)
 
         ag.signals_.finish_edit.connect(self.edit_finish)
-        self.collapse_item(False)
+        self.set_collapse_icon(False)
 
     @QtCore.pyqtSlot(int)
     def edit_finish(self, id: int):
         if self.id == id:
-            self.ui.textBrowser.setMarkdown(self.ui.textEdit.toPlainText())
+            self.ui.edit.setEnabled(True)
+            self.set_note_text(self.ui.textEdit.toPlainText())
             self.ui.textBrowser.show()
             self.ui.textEdit.hide()
 
     def set_note_text(self, note: str):
         self.ui.textBrowser.setMarkdown(note)
-        size = self.ui.textBrowser.document().size().toSize()
-        self.height_ = size.height() + self.ui.item_header.height()
-        logger.info(f'{size=}')
+        self.set_height_by_text()
         self.updateGeometry()
+
+    def set_height_by_text(self):
+        size = self.ui.textBrowser.document().size().toSize()
+        self.visible_height = size.height() + self.ui.item_header.height()
 
     def set_note_id(self, id: int):
         self.id = id
@@ -51,32 +59,38 @@ class Comment(QWidget):
     def get_note_id(self) -> int:
         return self.id
 
-    def set_height(self, height: int):
-        self.height_ = height
-        self.updateGeometry()
-
     def sizeHint(self) -> QtCore.QSize:
-        return QtCore.QSize(0, self.height_)
-
-    def get_heigh(self) -> int:
-        return self.height_
+        return QtCore.QSize(0, self.visible_height)
 
     @QtCore.pyqtSlot(bool)
     def collapse_item(self, state: bool):
+        active = self.ui.textBrowser if self.ui.edit.isEnabled() else self.ui.textEdit
+
         if state:
-            self.ui.textBrowser.hide()
-            self.ui.collapse.setIcon(icons.get_other_icon("right"))
+            self.expanded_height = self.height()
+            self.visible_height -= active.height()
+            active.hide()
         else:
-            self.ui.textBrowser.show()
-            self.ui.collapse.setIcon(icons.get_other_icon("down"))
+            self.visible_height = self.expanded_height
+            self.expanded_height = 0
+            active.show()
+        self.set_collapse_icon(state)
+
+    def set_collapse_icon(self, collapse: bool):
+        self.ui.collapse.setIcon(
+            icons.get_other_icon("right") if collapse
+            else icons.get_other_icon("down")
+        )
 
     @QtCore.pyqtSlot()
     def edit_note(self):
+        self.ui.edit.setEnabled(False)
         self.ui.textBrowser.hide()
         self.ui.textEdit.setMinimumHeight(self.ui.textBrowser.height()+20)
-        self.ui.textEdit.show()
         self.ui.textEdit.setText(self.ui.textBrowser.toPlainText())
+        self.ui.textEdit.show()
         self.start_edit.emit(self.id)
+        self.visible_height = self.height()
 
     @QtCore.pyqtSlot()
     def remove_note(self):
