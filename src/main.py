45b9c4c32801524b1e61bcdadf51524b70e53ac5
mihal.d44 <mihal.d44@gmail.com>
2023-10-08 06:33:13 +0300

    switching logging

--- src/main.py
+++ src/main.py
@@ -29,21 +29,36 @@ def tab_pressed():
     else:
         ag.dir_list.setFocus()
 
-def set_logger(file):
+def set_logger():
     logger.remove()
-    fmt = "{time:%b-%d %H:%M:%S} | {module}.{function}({line}): {message}"
+    use_logging = int(utils.get_app_setting("SWITCH_ON_LOGGING", 0))
+    if not use_logging:
+        return
 
-    if file == "sys.stderr":
+    fmt = "{time:%y-%b-%d %H:%M:%S} | {module}.{function}({line}): {message}"
+
+    std_err = int(utils.get_app_setting("LOGGING_TO_STDERR", 0))
+    if std_err:
         logger.add(sys.stderr, format=fmt)
     else:
-        logger.add(file, format=fmt)
-    logger.info("START ==============================>")
+        from datetime import datetime as dt
+        log_path = utils.get_app_setting("DEFAULT_LOG_PATH", "")
+        r_path = Path(log_path) if log_path else Path().resolve()
+        file_name = f"{dt.now():%b %d %H.%M.%S}.log"
+        file = r_path / file_name
+
+        logger.add(file.as_posix(), format=fmt)
     logger.info(f'{ag.app_name()=}, {ag.app_version()=}')
+    logger.info("START =================>")
 
 def instance_control(db_name: str):
+    global app
+    app = QApplication([])
+
     pid = iman.new_app_instance()
 
-    ag.single_instance = utils.get_app_setting("SINGLE_INSTANCE", False)
+    ag.single_instance = int(utils.get_app_setting("SINGLE_INSTANCE", 0))
+    logger.info(f'{db_name}, {pid=}, {ag.single_instance=}')
     if pid:
         if ag.single_instance:
             win_activate.activate(pid)
@@ -58,13 +73,10 @@ def instance_control(db_name: str):
 
 
 def start_app():
-    global app
-    app = QApplication([])
-
     thema_name = "default"
     try:
-        log_qss = utils.get_app_setting("LOG_QSS", False)
-        utils.apply_style(app, thema_name, to_save=log_qss) # type: ignore
+        log_qss = int(utils.get_app_setting("LOG_QSS", 0))
+        utils.apply_style(app, thema_name, to_save=log_qss)
     except KeyError as e:
         # message for developers
         logger.info(f"KeyError: {e.args}; >>> check you qss parameters file {thema_name}.param")
@@ -79,17 +91,12 @@ def start_app():
     sys.exit(app.exec())
 
 
-
 def main(entry_point: str, db_name: str):
-    # from datetime import datetime as dt
-    # file_name = f"fill-{dt.now():%b-%d-%H}.log"
-    # file_name = "sys.stderr"
-    # set_logger(file_name)
+    set_logger()
+    tmp = Path(entry_point).resolve()
     logger.info(entry_point)
-    tmp = Path(entry_point)
     if getattr(sys, "frozen", False):
-
-        ag.entry_point = (tmp.parent / 'fileo.exe').as_posix()
+        ag.entry_point = tmp.as_posix()   # str
     else:
         ag.entry_point = tmp.name
 
